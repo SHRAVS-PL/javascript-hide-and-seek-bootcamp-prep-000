7ee8e497099b6efc237a9a5e1ee7e813cb8c75d5
Shravani PL <pl.shravani@gmail.com>
2018-06-13 03:41:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  var sel = document.querySelector(selector)
+  return sel
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var elements = document.getElementById('app').querySelectorAll('.ranked-list')
+  for (var i = 0; i< elements.length;i++) {
+    elements[i].innerHTML =( i+n).toString()
+  }
+}
+
+function deepestChild() {
+ var ele = document.querySelectorAll('grand-node')
+  var a;
+  for(var i = 0; i < ele.length ; i++) {
+ if(i== ele.length - 1) {
+    a = ele[i]
+   }
+  }
+  return a
+}
